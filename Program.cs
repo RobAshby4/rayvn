485d6d95934b0f263eb7af129c055ba89adfa496
RobAshby4 <robert.n.ashby@gmail.com>
2024-09-09 15:26:15 -0700

    reads and displays text from test file. handles errors.

--- Program.cs
+++ Program.cs
@@ -1,17 +1,38 @@
 ﻿using Raylib_cs;
+using System.IO;
 
 class Program
 {
     public static void Main()
     {
+
         Raylib.InitWindow(800, 480, "Hello World");
+        StreamReader reader = new StreamReader("./testinput.txt");
+        string? text = "init text";
 
         while (!Raylib.WindowShouldClose())
         {
             Raylib.BeginDrawing();
             Raylib.ClearBackground(Color.White);
 
-            Raylib.DrawText("Hello, world!", 12, 12, 20, Color.Black);
+            if (Raylib.IsKeyPressed(KeyboardKey.Space))
+            {
+                try
+                {
+                    text = reader.ReadLine();
+                }
+                catch(IOException e)
+                {
+                    Console.WriteLine("IO Error: " + e.Message);
+                }
+            }
+
+            if (text == null)
+            {
+                break;
+            }
+
+            Raylib.DrawText(text, 12, 12, 20, Color.Black);
 
             Raylib.EndDrawing();
         }
